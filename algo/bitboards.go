17a17d925b402c89b115818b13e808ab68d4728a
Ideluis42 <ideluis@olin.edu>
2022-11-29 11:45:44 -0500

    started part 9

--- algo/bitboards.go
+++ algo/bitboards.go
@@ -29,7 +29,7 @@ func InitializeBoard(pos *Position, seq string) {
 
 func Play(pos *Position, col int) {
 	pos.position ^= pos.mask
-	pos.mask |= pos.mask + bottomMask(col)
+	pos.mask |= pos.mask + bottomMaskCol(col)
 	pos.moves++
 }
 
@@ -40,18 +40,18 @@ func Key(pos uint64, mask uint64) uint64 {
 func IsWinningMove(pos *Position, col int) bool {
 	test_pos := pos.position
 
-	test_pos |= (pos.mask + bottomMask(col)) & columnMask(col)
+	test_pos |= (pos.mask + bottomMaskCol(col)) & columnMask(col)
 	// fmt.Println("Test pos: ", strconv.FormatInt(int64(test_pos), 2))
 	return Aligned(test_pos)
 }
 
 // return a bitmask containing a single 1 corresponding to the top cell of a given column
-func topMask(col int) uint64 {
-	return (1 << 5) << (col * 7)
+func topMaskCol(col int) uint64 {
+	return 1 << (5 + (col * 7))
 }
 
 // return a bitmask containing a single 1 corresponding to the bottom cell of a given column
-func bottomMask(col int) uint64 {
+func bottomMaskCol(col int) uint64 {
 	return 1 << (col * 7)
 }
 
@@ -88,4 +88,36 @@ func Aligned(pos uint64) bool {
 	}
 
 	return false
+}
+
+func ComputeWinningPosition(pos *Position, sol *Solver) uint64 {
+	// vertical win
+	r := (pos.position << 1) & (pos.position << 2) & (pos.position << 3)
+
+	// horizontal win
+	p := (pos.position << 7) & (pos.position << 14)
+	r |= p & (pos.position << 21)
+	r |= p & (pos.position >> 7)
+	p >>= 21
+	r |= p & (pos.position << 7)
+	r |= p & (pos.position >> 21)
+
+	// diagonal down
+	p = (pos.position << 6) & (pos.position << 12)
+	r |= p & (pos.position << 18)
+	r |= p & (pos.position >> 6)
+	p >>= 18
+	r |= p & (pos.position << 6)
+	r |= p & (pos.position >> 18)
+
+	// diagonal up
+	p = (pos.position << 8) & (pos.position << 16)
+	r |= pos.position & (pos.position << 24)
+	r |= pos.position & (pos.position >> 8)
+	p >>= 24
+	r |= p & (pos.position << 8)
+	r |= p & (pos.position >> 24)
+
+	// need to finish return statement
+	return r
 }
