c460b65d488afaf3ca7736275be1a56174e9e449
Ideluis42 <ideluis@olin.edu>
2022-12-02 11:56:27 -0500

    adding compute winning pos

--- algo/bitboards.go
+++ algo/bitboards.go
@@ -27,17 +27,6 @@ func InitializeBoard(pos *Position, seq string) {
 	}
 }
 
-func InitializeBoard(pos *Position, seq string) {
-	for i := 0; i < len(seq); i++ {
-		colByte := seq[i] - '1'
-		// fmt.Println(colByte)
-		col := int(colByte)
-		if col < 0 || col > 7 || !CanPlay(pos, col) || IsWinningMove(pos, col) {
-			return
-		}
-		Play(pos, col)
-	}
-}
 func Play(pos *Position, col int) {
 	pos.position ^= pos.mask
 	pos.mask |= pos.mask + bottomMask(col)
@@ -99,4 +88,42 @@ func Aligned(pos uint64) bool {
 	}
 
 	return false
+}
+
+// returns the popcount of compute winning position
+func ComputeWinningPosition(pos *Position) uint64 {
+
+	// vertical wins
+	winningMovesBitmask := (pos.position << 1) & (pos.position << 2) & (pos.position << 3)
+
+	// horizontal win
+	// check for a win to the directly to the left
+	shiftedPosition := (pos.position << 7) & (pos.position << 14)
+	winningMovesBitmask |= shiftedPosition & (pos.position << 21)
+	// check for a win in the middle (1 1 0 1) where 0 is our winning space
+	winningMovesBitmask |= shiftedPosition & (pos.position >> 7)
+
+	// check for right wins
+	shiftedPosition = (pos.position >> 7) & (pos.position >> 14)
+	winningMovesBitmask |= shiftedPosition & (pos.position << 7)
+	winningMovesBitmask |= shiftedPosition & (pos.position >> 21)
+
+	// check for diagonal wins:
+
+	// downwards diagonal
+	shiftedPosition = (pos.position << 6) & (pos.position << 12)
+	winningMovesBitmask |= shiftedPosition & (pos.position << 18)
+	winningMovesBitmask |= shiftedPosition & (pos.position >> 6)
+
+	// upwards diagonal
+	shiftedPosition = (pos.position << 8) & (pos.position << 16)
+	winningMovesBitmask |= shiftedPosition & (pos.position << 24)
+	winningMovesBitmask |= shiftedPosition & (pos.position >> 8)
+
+	// to do finish function
+	// winningMovesBitmask &= // board_mask ^ mask
+	// popcount on winning moves bitmask
+	// return popcount
+
+	return 0
 }
