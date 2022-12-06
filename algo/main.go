d6b700c6a98d001b07cbade3c49527ea16f2d47a
Ideluis42 <ideluis@olin.edu>
2022-12-06 17:41:32 -0500

    commented exactly where changes need to be made

--- algo/main.go
+++ algo/main.go
@@ -1,23 +1,49 @@
 package main
 
-// build using go build -buildmode=c-shared -o main.so
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+)
 
 func main() {
-	Tester()
+	// Tester()
 	// init structs for pos and solver
 	position := newPosition()
 	solver := newSolver()
-
+	InitializeBoard(position, "2323445532")
+	var userInput int
+	pythonLine := "import serial; print(serial.to_serial("
 	for position.moves <= 49 {
+
 		// five steps:
 		// 1, Get user input
-		// 2, play user move with position bitboard
+		fmt.Print("User Input: ") // for debug purposes :D
+		fmt.Scanln(&userInput)
 
+		// check if user Input is winning move:
+		if IsWinningMove(position, userInput) {
+			fmt.Println("Opponent Wins")
+			break
+		}
+		// 2, play user move with position bitboard
+		Play(position, userInput)
 		// 3, run negamax and get column
-		col := Negamax(position, solver, -42, 42)
+		fmt.Println("Running Negamax")
+		col := Negamax(position, solver, -42, 42) // NEED TO EDIT HERE
 		// 4, run python function to move to column
-		print(col)
+		pythonLine += strconv.Itoa(col) + "))"
+
 		// 5, repeat
+		fmt.Println("Running Python")
+		cmd := exec.Command("python3", "-c", pythonLine)
+		// fmt.Println(cmd.Args)
+
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Column: ", string(out))
 	}
 
 }
---
+++ algo/serial.py
@@ -0,0 +1,10 @@
+# File to put python function to print to serial port
+# Need to debug since I never checked if this works with arduino
+# Should only WRITE to serial port and arduino will only READ to serial port
+
+import serial
+def to_serial(col):
+    # port = ""
+    # s = serial.Serial(port)
+    # s.write(col)
+    return col
